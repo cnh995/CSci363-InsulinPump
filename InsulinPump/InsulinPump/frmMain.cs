98b2b4f9f131f79c98328184ea4553bb3d0276d5
NinjaA55a5in13 <34103982+NinjaA55a5in13@users.noreply.github.com>
2017-12-01 01:18:51 -0600

    I think im doing this right
    hopefully

--- InsulinPump/InsulinPump/frmMain.cs
+++ InsulinPump/InsulinPump/frmMain.cs
@@ -12,9 +12,79 @@ namespace InsulinPump
 {
     public partial class frmMain : Form
     {
+        public static int dosage;
+        public static string doseDate;
+        public static string doseTime;
+        public static int insulinRemaining;
+        public static int bloodSugar;
+        public static int batteryLevel;
+        public static Boolean fromFrmScheduleDose = false;
+        public static frmMain gfrmMain = new frmMain();
+
         public frmMain()
         {
             InitializeComponent();
+
+        }
+
+        private void btnScheduleDose_Click(object sender, EventArgs e)
+        {
+            frmScheduleDose lfrmScheduleDose = new frmScheduleDose();
+            lfrmScheduleDose.Show();
+            lfrmScheduleDose.Location = this.Location;
+            this.Hide();
+        }
+
+        private void btnReadingHistory_Click(object sender, EventArgs e)
+        {
+            frmReadingHistory lfrmReadingHistory = new frmReadingHistory();
+            lfrmReadingHistory.Show();
+            lfrmReadingHistory.Location = this.Location;
+            this.Hide();
+        }
+
+        private void lowBatteryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your battery is low, please charge your device.", "Low Battery");
+        }
+
+        private void lowInsulinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your insulin cartridge is low, please refill your insulin.", "Low Insulin");
+        }
+
+        private void replaceNeedleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your needle must be replaced.", "Replace Needle");
+        }
+
+        private void highBloodSugarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your blood sugar is too high, please schedule an insulin dose.", "High Blood Sugar");
+        }
+
+        private void lowBloodSugarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your blood sugar is too low, please eat to increase your blood sugar.", "Low Blood Sugar");
+        }
+
+        private void disconnectedToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Your device has disconnected, please reconnect or manually monitor your blood sugar.", "Device Disconnected");
+        }
+
+        private void insulinToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            if (fromFrmScheduleDose)
+            {
+                MessageBox.Show(dosage.ToString() + doseDate + doseTime);
+                fromFrmScheduleDose = false;
+            }
         }
     }
 }
